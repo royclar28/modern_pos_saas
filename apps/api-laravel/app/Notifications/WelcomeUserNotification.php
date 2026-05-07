a963df8679819dc4d1c5acdc33a9d72d51f566a2
royclar28 <roydanielcalderon28@gmail.com>
2026-05-06 21:43:29 -0400

    fix: enviar correo de bienvenida de forma sincrona

--- apps/api-laravel/app/Notifications/WelcomeUserNotification.php
+++ apps/api-laravel/app/Notifications/WelcomeUserNotification.php
@@ -3,11 +3,11 @@
 namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
+
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class WelcomeUserNotification extends Notification implements ShouldQueue
+class WelcomeUserNotification extends Notification
 {
     use Queueable;
 
